8bc143567a950ed5ca12b4278cb8392b9e05363c
masahiko <koba58840527@gmail.com>
2018-08-11 18:11:09 +0900

    add description

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 echo "hello world";
 echo "<br>";
-echo "today is 2018.08.11 17:17";
+echo "today is 2018.08.11 18:02";
 echo "<br>";
 echo "gitのテストの為に作成したプロジェクトです。最終的にはjenkinsでリリースしたい";
+echo "<br>";
+echo "リモートにしかないファイルがある場合どうなる？";
